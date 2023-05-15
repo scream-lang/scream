b7924557948d757682260763d905f3c21e6af0a8
Timo Sarkar <sarkar.timo@gmail.com>
2023-05-15 15:49:16 +0200

    refactoring codebase

--- default.nix
+++
@@ -1,4 +0,0 @@
-{ pkgs ? import <nixpkgs> { }, lib ? pkgs.lib
-, fetchFromGitHub ? pkgs.fetchFromGitHub, ncurses ? pkgs.ncurses
-, cmake ? pkgs.cmake, libxml2 ? pkgs.libxml2, symlinkJoin ? pkgs.symlinkJoin
-, cudaPackages ? pkgs.cudaPackages, enableCUDA ? false }:
