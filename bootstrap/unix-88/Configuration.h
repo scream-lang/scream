345497eefcd642c0f13b46b53295d47738cefbc0
0xtimosarkar <sartimo10gmail.com>
2023-11-12 15:41:52 +0100

    added bootstrap compiler for scream based on oberon-2

---
+++ bootstrap/unix-88/Configuration.h
@@ -0,0 +1,15 @@
+/* scream 2.1.0 [2022/03/15]. Bootstrapping compiler for address size 8, alignment 8. xrtspaSF */
+
+#ifndef Configuration__h
+#define Configuration__h
+
+#include "SYSTEM.h"
+
+
+import CHAR Configuration_versionLong[76];
+
+
+import void *Configuration__init(void);
+
+
+#endif // Configuration
---
+++ bootstrap/unix-88/OPP.h
@@ -0,0 +1,16 @@
+/* scream 2.1.0 [2022/03/15]. Bootstrapping compiler for address size 8, alignment 8. xrtspaSF */
+
+#ifndef OPP__h
+#define OPP__h
+
+#include "SYSTEM.h"
+#include "OPT.h"
+
+
+
+
+import void OPP_Module (OPT_Node *prog, UINT32 opt);
+import void *OPP__init(void);
+
+
+#endif // OPP
